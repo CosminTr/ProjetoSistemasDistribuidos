f2707edb60ac97778102f5db313082df57280b33
Beatriz Silva <104373944+bia-silva99@users.noreply.github.com>
2022-10-03 18:32:16 +0100

    Update tree-private.h

--- include/tree-private.h
+++ include/tree-private.h
@@ -11,6 +11,16 @@ struct tree_t {
 	struct tree_t *right;
 };
 
+struct tree_t *minValNode(strct tree_t *node){
+	struct tree_t* current = node;
+  
+    /* loop down to find the leftmost leaf */
+    while (current && current->left != NULL)
+        current = current->left;
+  
+    return current;
+};
+
 int tree_put_recursive(struct tree_t *tree, struct entry_t *entry);
 
 void get_keys_recursive(struct tree_t *tree, int *positionCounter, char **key_list);
