b7923366a87d1dacc36ebdb4597e2227dd62ab2a
joaoinca <joaoinca007@gmail.com>
2022-10-20 20:33:38 +0100

    scuffed send receive, errors, check discord

--- grupo19/source/network_client.c
+++ grupo19/source/network_client.c
@@ -29,6 +29,49 @@ int network_connect(struct rtree_t *rtree){
 }
 
 struct message_t *network_send_receive(struct rtree_t * rtree, struct message_t *msg){
+    int msglen = message_t__get_packed_size(&msg->message);
+    int resposta_len;
+    uint8_t *buffer = malloc(msglen);
+    //send
+    message_t__pack(&msg->message, buffer);
+    int netlong = htonl(msglen);
+    write(rtree->socket_num, &netlong, sizeof(int));
+
+    int resultado;
+
+    while (msglen > 0) {
+        int resultado = write(rtree->socket_num, buffer, msglen);
+        if (resultado < 0) {
+            //deu erro
+
+        }
+        msglen = msglen - resultado;
+        buffer = buffer + resultado;
+    }
+
+    free(buffer);
+
+    //receive
+    read(rtree->socket_num, &resposta_len, sizeof(int));
+    resposta_len = ntohl(resposta_len);
+    uint8_t resp[resposta_len];
+
+    int index = 0;
+    int resultado;
+    while (index < resposta_len) {
+        resultado = read(rtree->socket_num, resp + index, resposta_len - index);
+        if (resultado < 1) {
+            //erro
+
+        }
+        index = index + resultado;
+
+    }
+    resp[resposta_len] = '\0';
+
+    //CONFUSAO AQUI
+    message_t *mensagem = message_t__unpack(NULL, resposta_len, resp);
+    msg->message = *mensagem;
     return NULL;
 }
 
