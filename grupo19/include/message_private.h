483936db55e9f4ab27a097a5823e4c0e3299ef73
Cosmin Trandafir <minut1203@gmail.com>
2022-11-11 11:58:45 +0000

    request_t reposition

--- grupo19/include/message_private.h
+++ grupo19/include/message_private.h
@@ -1,15 +1,6 @@
 #include "inet.h"
 #include "sdmessage.pb-c.h"
 
-struct request_t {
-    int op_n;       //o número da operação
-    int op;         //a operação a executar. op=0 se for um delete, op=1 se for um put
-    char* key;      //a chave a remover ou adicionar
-    char* data;     // os dados a adicionar em caso de put, ou NULL em caso de delete
-    //adicionar campo(s) necessário(s) para implementar fila do tipo produtor/consumidor
-    struct request_t *next;
-};
-
 MessageT *message_create();
 
 int write_all(int socket_num, uint8_t *buffer, int len);
--- grupo19/include/tree_skel.h
+++ grupo19/include/tree_skel.h
@@ -5,6 +5,15 @@
 #include "tree.h"
 #include "message_private.h"
 
+struct request_t {
+    int op_n;       //o número da operação
+    int op;         //a operação a executar. op=0 se for um delete, op=1 se for um put
+    char* key;      //a chave a remover ou adicionar
+    struct data_t* data;     // os dados a adicionar em caso de put, ou NULL em caso de delete
+    //adicionar campo(s) necessário(s) para implementar fila do tipo produtor/consumidor
+    struct request_t *next;
+};
+
 /* Inicia o skeleton da árvore.
  * O main() do servidor deve chamar esta função antes de poder usar a
  * função invoke(). 
